0e9bec632ad9171604d2f46fe45bf237bd6b758f
David Yoder <david.yoder@live.com>
2024-07-05 10:21:10 -0400

    made changes for missing images
    @kawilliams

--- script.js
+++ script.js
@@ -4,12 +4,38 @@ function openModal(element, type) {
     var modalImg = document.getElementById("modalImg");
     var modalVideo = document.getElementById("modalVideo");
     
-    modal.style.display = "block";
+    modal.style.display = "flex";
+    modal.style.alignItems = "center";
+    modal.style.justifyContent = "center";
     
     if (type === 'image') {
         modalImg.style.display = "block";
         modalVideo.style.display = "none";
-        modalImg.src = element.src;
+        modalImg.src = element.getAttribute('data-full-image') || element.src;
+        
+        // Reset dimensions
+        modalImg.style.width = 'auto';
+        modalImg.style.height = 'auto';
+        
+        // Wait for the image to load
+        modalImg.onload = function() {
+            var imgRatio = this.naturalWidth / this.naturalHeight;
+            var windowRatio = window.innerWidth / window.innerHeight;
+            
+            if (imgRatio > windowRatio) {
+                // Image is wider than the window
+                if (this.naturalWidth > window.innerWidth * 0.9) {
+                    this.style.width = '90%';
+                    this.style.height = 'auto';
+                }
+            } else {
+                // Image is taller than the window
+                if (this.naturalHeight > window.innerHeight * 0.9) {
+                    this.style.height = '90vh';
+                    this.style.width = 'auto';
+                }
+            }
+        };
     } else if (type === 'video') {
         modalImg.style.display = "none";
         modalVideo.style.display = "block";
